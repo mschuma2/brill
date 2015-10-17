c0e7988c66eeac998ecf79879b1ca0c9b5f11c81
mschuma2 <mschuma2@gmail.com>
2015-10-17 22:07:22 +0200

    add transparent logos

--- logos.bat
+++ logos.bat
@@ -1,28 +1,42 @@
 @echo off
 
-set AUTOHAUS = "false"
+set AUTOHAUS=false
 IF %1.==. GOTO No1
 IF %2.==. GOTO No2
-IF %3.==. set AUTOHAUS="true"
+IF NOT [%3]==[] set AUTOHAUS=true
 set OLDDIR=%CD%
 set TEMPLATE=%CD%
 
-:conv
+:prepare
 cd %1
-set LOGO=%TEMPLATE%\%2.jpg
-IF %AUTOHAUS% == "false" (
-	 set LOGO2=%TEMPLATE%\%3.jpg
-	 ECHO Nutze Logo-Vorlage: %LOGO2% 
-	 ) 
 
+set BILD=DSC_0001.jpg
+ECHO Bearbeite Bild %BILD%
+
+set LOGO=%TEMPLATE%\%2.png
 ECHO Nutze Logo-Vorlage: %LOGO% 
 
- ECHO Bearbeite Bild %%p
- IF %AUTOHAUS% == "false" ( 
-	convert -verbose %%p -page %LOGO% -flatten %%p
+
+for /f  %%i in ('identify -format "%%w" %BILD% ') do  set WIDTH=%%i
+ECHO Breite des Bildes %BILD%: %WIDTH%px
+
+echo Skaliere das Logo auf %WIDTH%px
+convert %LOGO% -resize %WIDTH% sizelogo.png
+
+echo Benutze zweites Logo: %AUTOHAUS%
+IF %AUTOHAUS%==true (
+	 set LOGO2=%TEMPLATE%\%3.png
+	 ECHO Nutze Logo-Vorlage: %LOGO2% 
+	 convert %LOGO% -resize %WIDTH% sizelogo2.png
+) 
+
+:conv
+ IF %AUTOHAUS%==false ( 
+	convert -verbose %BILD% -page +0+0 sizelogo.png -flatten out.jpg
+	del sizelogo.png
  ) ELSE ( 
-    set LOGO2=%TEMPLATE%\%3.jpg
-	convert -verbose %%p -page %LOGO% -page %LOGO2%  -flatten %%p
+	convert -verbose %BILD% -page +0+0 sizelogo.png -page +0+0 sizelogo2.png  -flatten out.jpg
+	del sizelogo2.png
  )  
 GOTO end
 
@@ -34,9 +48,6 @@ GOTO end
 :No2
   ECHO Keine Logo-Vorlage angegeben
 GOTO end
-:No3
-  ECHO Kein Autohaus angegeben
- GOTO end
 :end
 cd %OLDDIR%
  echo Konvertierung abgeschlossen.
